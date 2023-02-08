030f6a335c8e8689e72b3709543d53f0589e38a5
Nathan Seidle <nathan@sparkfun.com>
2023-02-08 13:17:21 -0700

    Fix export compiled binary to export 'with_bootloader'. Add missing bootloader HEX files. Fix boards.txt.

---
+++ sparkfun/samd/bootloaders/LoRaSerial/sparkfun-samd21-loraserial/board_config.h
@@ -0,0 +1,15 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define VENDOR_NAME "SparkFun"
+#define PRODUCT_NAME "SAMD21 LoRaSerial"
+#define VOLUME_LABEL "SPARKFUN"
+#define INDEX_URL "https://www.sparkfun.com/products/19311"
+#define BOARD_ID "SAMD21G18A-LoRaSerial-v1"
+
+#define USB_VID 0x1B4F
+#define USB_PID 0x002A
+
+#define LED_PIN PIN_PA27
+
+#endif
